7c9c1523632db6c2232747725211616ec2a607e2
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-03-03 15:59:57 -0500

    Getting closer to breaking things. Nothing ruined yet...

--- to-do-list.js
+++ to-do-list.js
@@ -4,9 +4,36 @@
     document.body.appendChild(btn);
   } */
 
-//   Second try
-function addToList() {
+// Second try
+/* function addToList() {
     var para = document.createElement("P");
     para.innerText = "This is a paragraph.";
     document.body.appendChild(para);
-  }
+  } */
+
+  // Third try is just a copy/paste
+function addDream() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("dreamBox").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("bucketList").appendChild(li);
+    }
+    document.getElementById("dreamBox").value = "";
+  
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  } 
